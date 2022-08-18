9a026c3efbf5835130c76e9942f155858608bcee
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-08-18 00:08:27 -0400

    add more things to code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let edad: u8 = 25;
-    println!("Hola mi nombre es Eduardo y tengo {} años de edad", edad);
+    let nombre: &str  =  "Eduardo";
+    let edad:   u8    =  25;
+    let lugar:  &str  =  "Merida";
+    println!("Hola mi nombre es {}, tengo {} años de edad, y vivo en {}.", nombre, edad, lugar);
 }
