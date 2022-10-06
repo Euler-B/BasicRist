06f18740afd16a9a024a54482f44d591c6b4b017
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-06 18:49:20 -0400

    add code for apply loop cycle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 fn main() {
-    let nombre: &str  =  "Eduardo";
-    let edad:   u8    =  25;
-    let lugar:  &str  =  "Merida";
-    println!("Hola mi nombre es {}, tengo {} años de edad, y vivo en {}.", nombre, edad, lugar);
+    // Declaramos 2 numeros que vamos a sumar 
+    let numero_1 = 123;
+    let numero_2 = 321;
+
+    let suma = numero_1 + numero_2;
+
+    loop {
+    // Mostrar los dos numeros en pantalla 
+        println!("Por favor escribir la suma de {} y {}: ", numero_1, numero_2);
+
+    // Obtener del usuario el numero que representa la suma.
+        let mut suma_usuario = String::new();
+        std::io::stdin().read_line(&mut suma_usuario).unwrap();
+    
+        let suma_usuario_int : i32 = suma_usuario.trim().parse().unwrap();
+
+        if suma_usuario_int == suma {
+            println!("Lo has hecho muy bien, el resultado {} es correcto", suma);
+            break;
+        } else {
+            println!("El resultado {} no es correcto, por favor intentalo de nuevo", suma_usuario_int);
+        }
+}
+
 }
