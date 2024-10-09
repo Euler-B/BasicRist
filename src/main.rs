f39328df3fc0e70b6441594eea8f910f5b031f09
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2024-10-09 00:14:54 -0400

    agregado mas enfasis, mejor lectura, y ejecucion del codigo

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,4 @@
 fn main() {
-    // Declaramos 2 numeros que vamos a sumar 
-    let numero_1 = 123;
-    let numero_2 = 321;
-
-    let suma = numero_1 + numero_2;
-
-    loop {
-    // Mostrar los dos numeros en pantalla 
-        println!("Por favor escribir la suma de {} y {}: ", numero_1, numero_2);
-
-    // Obtener del usuario el numero que representa la suma.
-        let mut suma_usuario = String::new();
-        std::io::stdin().read_line(&mut suma_usuario).unwrap();
-    
-        let suma_usuario_int : i32 = suma_usuario.trim().parse().unwrap();
-
-        if suma_usuario_int == suma {
-            println!("Lo has hecho muy bien, el resultado {} es correcto", suma);
-            break;
-        } else {
-            println!("El resultado {} no es correcto, por favor intentalo de nuevo", suma_usuario_int);
-        }
-    }
-
     // Obtengo el nombre del usuario
     println!("Hola, Buenas tardes cual es tu nombre:");
     let mut nombre: String = String::new();
@@ -30,13 +6,25 @@ fn main() {
     nombre = nombre.trim().to_string();
 
     // Voy  a obtener la edad del usuario
-    println!("Ahora me podrias decir cual es tu edad:");
-    let mut edad: String = String::new();
-    std::io::stdin().read_line(&mut edad).unwrap();
+    println!("Ahora me podrias decir cual es tu año de nacimiento:");
+    let mut año_nacimiento: String = String::new();
+    std::io::stdin().read_line(&mut año_nacimiento).unwrap();
+    let año_nacimiento_int: i32 = año_nacimiento.trim().parse().unwrap();
 
     //convierto la edad en entero
-    let  edad_int: u8 = edad.trim().parse().unwrap();
+    let año_actual = 2024;
+    let edad = año_actual - año_nacimiento_int;
 
     //Muestro la informacion en base a los datos que he recibido del usuario
-    println!("Hola Sr. {}, usted tiene {} años de edad", nombre, edad_int);
+    println!("Hola Sr. {}, usted tiene {} años de edad", nombre, edad);
+
+    if año_nacimiento_int <= 2006 {
+        println!("Ademas es de la Generacion Z");
+    } else if año_nacimiento_int <= 1996 {
+        println!("Ademas es de la Generacion Y");
+    } else if año_nacimiento_int <= 1980 {
+        println!("Ademas es de la Generacion X");
+    } else {
+        println!("Ademas es de la Generacion Baby Boomer");
+    }
 }
